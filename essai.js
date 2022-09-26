a1e976d062184e333184b5b572b69b6e6bf8343d
Glacoon <53223848+Glacoon@users.noreply.github.com>
2022-09-26 23:38:20 +0200

    Dépot de mon horloge à fuseau horaire
    Premier dépot de la V1 de mon horloge à fuseau horaire

---
+++ essai.js
@@ -0,0 +1,56 @@
+
+function showTime(){
+    const date = new Date();
+
+    let minute = date.getMinutes();
+    let seconde = date.getSeconds();
+    let heure = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+    let amPm = date.getHours() >= 12 ? "PM" : "AM";
+
+    minute = minute <10 ? "0" + minute : minute;
+    seconde = seconde <10 ? "0" + seconde : seconde;
+    document.getElementById("clock").innerHTML = `${heure}:${minute}:${seconde} ${amPm}`;
+}
+
+console.log("Hello World");
+let interval=setInterval(showTime, 1000);
+
+
+function changementPolice(){
+    var e = document.getElementById('clock');
+    if(e.style.fontFamily == "Montserrat, sans-serif"){
+        e.style.fontFamily = "DS-Digital";
+        e.style.fontSize= "173px";
+    }
+    else{
+        e.style.fontFamily = "Montserrat, sans-serif";
+        e.style.fontSize= "150px";
+    }
+}
+
+function changementTimeZone(){
+    const date = new Date();
+
+    let minute = date.getMinutes();
+    let seconde = date.getSeconds();
+    let heure = date.getHours() > 12 ? date.getHours() - 14 : date.getHours() - 2;
+    if(heure == -1){
+        heure = 11;
+    }
+    let amPm = date.getHours() >= 12 ? "AM" : "PM";
+
+    minute = minute <10 ? "0" + minute : minute;
+    seconde = seconde <10 ? "0" + seconde : seconde;
+
+    document.getElementById("clock").innerHTML = `${heure}:${minute}:${seconde} ${amPm}`;
+
+}
+
+function execution(){
+    clearInterval(interval);
+    let interval2 = setInterval(changementTimeZone, 1000);
+    setTimeout(function(){
+        clearInterval(interval2);
+        interval = setInterval(showTime, 1000);
+    }, 5000);
+}
